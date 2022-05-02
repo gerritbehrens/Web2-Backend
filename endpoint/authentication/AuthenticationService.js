dc32168aceaa128a6bbafc50a06faf3780349047
s50551 <s50551@bht-berlin.de>
2022-05-02 15:29:39 +0200

    update

--- endpoint/authentication/AuthenticationService.js
+++ endpoint/authentication/AuthenticationService.js
@@ -4,56 +4,6 @@ var jwt = require('jsonwebtoken')
 var axios = require('axios');
 const User = require('../user/UserModel');
 
-function createSessionToken(props, callback) {
-    console.log("AuthentificationService: create token")
-
-    if (!props) {
-        console.log("Error: have no json body")
-        callback("JSON-Body missing", null, null)
-        return
-    }
-
-    userService.searchUser(props.userID, function (err, user) {
-        console.log("I am searching a user to authenticate")
-        if (user) {
-            console.log("Found user, check password!")
-
-            user.comparePassword(props.password, function (err, isMatch) {
-                if (err) {
-                    console.log("Password is invalid");
-                    return callback(err, null)
-                }
-                else {
-                    if (isMatch) {
-                        console.log("Password is correct. Create token.")
-
-                        var issuedAt = new Date().getTime();
-                        var expirationTime = config.get('session.timeout')
-                        var expiresAt = issuedAt + (expirationTime * 1000)
-                        var privateKey = config.get('session.tokenKey')
-                        let token = jwt.sign({ "user": user.userID }, privateKey, { expiresIn: expiresAt, algorithm: 'HS256' })
-
-                        console.log("Token created: " + token)
-                        return callback(null, token, user)
-                    }
-                    else {
-                        console.log("Password or userID are invalid.")
-                        return callback(err, null)
-                    }
-
-                }
-            })
-
-        }
-        else {
-            return callback(err, null)
-        }
-
-    })
-
-
-}
-
 function authenticate(userID, password, callback) {
     userService.searchUser(userID, function (err, user) {
         console.log("I am searching a user to authenticate")
@@ -94,6 +44,5 @@ function authenticate(userID, password, callback) {
 }
 
 module.exports = {
-    createSessionToken,
     authenticate
 }
